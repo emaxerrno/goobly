6e7e4b652fda06e2383751c4131fa21f4050aad5
agallego <gallego.alexx@gmail.com>
2015-11-20 11:36:01 -0500

    fixed compilation

--- src/rocksdb_state/options_utils.hpp
+++ src/rocksdb_state/options_utils.hpp
@@ -1,5 +1,5 @@
 #pragma once
-
+#include <rocksdb/db.h>
 namespace goobly {
 inline ::rocksdb::Options dboptsFactory() {
   rocksdb::Options o{};
@@ -12,22 +12,23 @@ inline ::rocksdb::Options dboptsFactory() {
   o.OptimizeLevelStyleCompaction();
   // rocksdb mailing list optimization for queues
   // https://www.facebook.com/groups/rocksdb.dev
-  o.setMaxOpenFiles(-1);
+  o.max_open_files = -1;
   o.create_if_missing = true;
   o.create_missing_column_families = true;
   o.allow_mmap_reads = true;
   o.allow_mmap_writes = true;
   o.access_hint_on_compaction_start = rocksdb::DBOptions::SEQUENTIAL;
-  // TODO(add block table config & use bloomfilter)
-  // final BlockBasedTableConfig tableOptions = new BlockBasedTableConfig();
-  // tableOptions.setFilter(new BloomFilter(10, false));
-  // options.setTableFormatConfig(tableOptions);
-  // Please make sure you set up BloomFilter with parameter useBlockBasedMode=false
+  rocksdb::BlockBasedTableOptions table_options;
+  table_options.filter_policy.reset(rocksdb::NewBloomFilterPolicy(10, false));
+  o.table_factory.reset(rocksdb::NewBlockBasedTableFactory(table_options));
   return o;
 }
 
 inline ::rocksdb::WriteOptions woptsFactory() {
   rocksdb::WriteOptions o{};
+  // Note: If you don't end up using rocksdb for the index, then definitely
+  // set this to false as you already have the write-ahead-log entry synced
+  // on disk
   o.sync = true;
   return o;
 }
