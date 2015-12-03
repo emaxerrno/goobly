58d176ce95d99ea92d3e4fb110be2a4ed56117aa
agallego <gallego.alexx@gmail.com>
2015-12-03 18:17:36 -0500

    Compiling gbly binary w/ dynamic libs

--- src/goobly_db/main.cc
+++ src/goobly_db/main.cc
@@ -1,4 +1,9 @@
 #include <iostream>
-int main(int args, char** argv, char** env){
+#include <grpc++/grpc++.h>
+#include "goobly.grpc.pb.h"
+int main(int args, char *argv[], char *env[]) {
+  // Verify that the version of the library that we linked against is
+  // compatible with the version of the headers we compiled against.
+  GOOGLE_PROTOBUF_VERIFY_VERSION;
   std::cout << "Hello world" << std::endl;
 }
